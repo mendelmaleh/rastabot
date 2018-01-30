d5483499a8c85e5253585411f88fce55406ad079
Mendel <mendelmaleh@gmail.com>
2018-01-30 02:14:51 +0200

    first commit

---
+++ handler.py
@@ -0,0 +1,33 @@
+import json
+import os
+import sys
+
+here = os.path.dirname(os.path.realpath(__file__))
+sys.path.append(os.path.join(here, "./vendored"))
+
+import requests
+
+token = "516160274:AAEgyfeASRRdb0_XFDxtAOTtxQsjJjkt1bo"
+baseURL = "https://api.telegram.org/bot{}".format(token)
+
+
+def hello(event, context):
+    try:
+        data = json.loads(event["body"])
+        message = str(data["message"]["text"])
+        chat_id = data["message"]["chat"]["id"]
+        first_name = data["message"]["chat"]["first_name"]
+
+        response = "Please /start, {}".format(first_name)
+
+        if "start" in message:
+            response = "Hello {}".format(first_name)
+
+        data = {"text": response.encode("utf8"), "chat_id": chat_id}
+        url = baseURL + "/sendMessage"
+        requests.post(url, data)
+
+    except Exception as e:
+        print(e)
+
+    return {"statusCode": 200}
